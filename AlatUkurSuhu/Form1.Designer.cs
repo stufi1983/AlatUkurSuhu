326a2d98b9519b513d379b3f5ecd668c63001c79
stufi1983 <winda1984>
2016-12-15 09:05:40 +0700

    First Commit

---
+++ AlatUkurSuhu/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace AlatUkurSuhu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                try
+                {
+                    components.Dispose();
+                }
+                catch  { }
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(84, 301);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Koneksi";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(6, 139);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Mulai";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "5",
+            "15",
+            "60"});
+            this.comboBox2.Location = new System.Drawing.Point(7, 112);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(70, 21);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Interval(detik):";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 47);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(70, 46);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Visible = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(71, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 346);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(784, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(42, 17);
+            this.toolStripStatusLabel1.Text = "Status:";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(16, 17);
+            this.toolStripStatusLabel2.Text = "...";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Maximum = 10;
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 256);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Simpan Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(94, 22);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(684, 283);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(8, 168);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(52, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Scroll";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(7, 214);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(68, 36);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Bersihkan Data";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 50;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Data hasil pengukuran:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 368);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Alat ukur suhu beton";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ AlatUkurSuhu/Form1.cs
@@ -0,0 +1,382 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+using System.IO;
+using System.Xml;
+
+using System.IO.Ports;
+using System.Management;
+using OfficeOpenXml;
+
+namespace AlatUkurSuhu
+{
+    public partial class Form1 : Form
+    {
+        string[] ports;
+        int portscount = 0;
+
+        bool receiveing = false, closing = false;
+
+        //Add items in the listview
+        string[] arr = new string[17];
+        ListViewItem itm;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string CommText="Arduino";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            serialPort1.ReadTimeout = 1000;
+            serialPort1.WriteTimeout = 1000;
+
+            timer1.Enabled = true;
+            toolStripStatusLabel2.Text = "Alat tidak terhubung";
+
+            comboBox2.SelectedIndex = 0;
+
+            listView1.Columns.Add("Waktu", 50);
+            for (int s = 1; s < 16; s++)
+            {
+                listView1.Columns.Add("S " + s.ToString(),41);
+            }
+
+
+            string path = System.IO.Directory.GetCurrentDirectory() + @"\koneksi.txt";
+            if (File.Exists(path))
+            {
+                try
+                {
+                    string[] FileContents = System.IO.File.ReadAllLines(path);
+                    CommText = FileContents[0];
+                }
+                catch { }
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ports = SerialPort.GetPortNames();
+
+            if (ports.Length != portscount)
+            {
+                portscount = ports.Length;
+                comboBox1.Items.Clear();
+                comboBox1.Text = "";
+
+                using (var searcher = new ManagementObjectSearcher("SELECT * FROM WIN32_SerialPort"))
+                {
+                    string[] portnames = SerialPort.GetPortNames();
+                    var portss = searcher.Get().Cast<ManagementBaseObject>().ToList();
+                    var tList = (from n in portnames join p in portss on n equals p["DeviceID"].ToString() select n + " - " + p["Caption"]).ToList();
+
+                    foreach (string s in tList)
+                    {
+                        if (s.Contains(CommText))
+                        {
+                            String[] portName = s.Split(' ');
+                            comboBox1.Items.Add(portName[0]);
+                            comboBox1.SelectedIndex = 0;
+                        }
+
+                    }
+                }
+                if (comboBox1.Items.Count > 0) {
+                    serialPort1.PortName = comboBox1.Text;
+                    try
+                    {
+                        if (serialPort1.IsOpen == false) //if not open, open the port
+                        {
+                            serialPort1.Open();
+                            toolStripStatusLabel2.Text = "Alat telah terhubung";
+                            button2.Enabled = true;
+                            timer2.Interval = 1000;
+                            timer2.Start();
+                        }
+                    }
+                    catch (Exception err) { }
+                }
+            }
+        }
+
+        String SerialBuffer = "";
+        int dataCount = -1;
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            receiveing = true;
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            SetText(indata);
+
+            dataCount++; if (dataCount > 65535) dataCount = 0;
+
+            if (closing) { serialPort1.Close();  }
+        }
+
+        delegate void SetTextCallback(string text);
+        private void SetText(string text)
+        {
+            if (this.textBox1.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                try
+                {
+                    this.Invoke(d, new object[] { text });
+                }
+                catch {
+                    return;
+                }
+            }
+            else
+            {
+                if (text.Contains('$'))
+                {
+                    String tampilkan = SerialBuffer + text;
+                    int index = tampilkan.IndexOf("#");
+                    tampilkan = tampilkan.Substring(index);
+                    this.textBox1.Text = tampilkan;//.Trim('$');
+                    SerialBuffer = text;
+                }
+                else
+                    SerialBuffer += text;
+                
+                textBox1.SelectionStart = textBox1.Text.Length;
+                textBox1.ScrollToCaret();
+
+            }
+            receiveing = false;
+
+            String[] x = textBox1.Text.Split('$');
+
+
+       }
+        int lastDataCount = 0;
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+            if (closing) { timer2.Enabled = false; this.Close(); return; }
+            if (lastDataCount != dataCount)
+            {
+                lastDataCount = dataCount;
+
+            } else {
+                timer2.Enabled = false;
+                toolStripStatusLabel2.Text = "Alat tidak terhubung";
+
+                button2.Enabled = false;
+                timer4.Enabled = false;
+
+                MessageBox.Show("Sambungan ke alat terputus. Jalankan program kembali untuk pengambilan data!", "Perhatian", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            closing = true;
+            if (receiveing) { e.Cancel = true; return; }
+            timer1.Enabled = false; timer2.Enabled = false;
+            try
+            {
+                serialPort1.Close();
+                serialPort1.Dispose();
+            }
+            catch { }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Excel 2007 |*.xlsx";
+            saveFileDialog1.Title = "Save to Excel File";
+            saveFileDialog1.ShowDialog();
+
+            if (saveFileDialog1.FileName != "")
+            {
+                switch (saveFileDialog1.FilterIndex)
+                {
+                    case 1:
+                        FileInfo newFile = new FileInfo(saveFileDialog1.FileName);
+                        if (newFile.Exists)
+                        {
+                            try
+                            {
+                                newFile.Delete();  // ensures we create a new workbook
+                                newFile = new FileInfo(saveFileDialog1.FileName);
+                            }
+                            catch {
+                                MessageBox.Show("Pembuatan file gagal. Pastikan file tidak digunakan");
+                                return;
+                            }
+                        }
+
+                        using (ExcelPackage xlPackage = new ExcelPackage(newFile))
+                        {
+                            xlPackage.DebugMode = true;
+
+                            ExcelWorksheet worksheet = xlPackage.Workbook.Worksheets.Add("Tinned Goods");
+
+                            //Title
+                            worksheet.Cell(1, 1).Value = "Waktu";
+                            for (int s = 1; s < 16; s++)
+                            {
+                                worksheet.Cell(1, s+1).Value = "S "+s.ToString();
+                                worksheet.Column(s+1).Width = 5;
+                            }
+
+                            xxx = new string[listView1.Items.Count, 16];
+                            for (int y = 0; y < listView1.Items.Count; y++)
+                            {
+                                for (int x = 0; x < 16; x++)
+                                {
+                                    //xxx[y, x] = listView1.Items[y].SubItems[x].Text;
+
+                                    //y = 1, title
+                                    worksheet.Cell(y+2, x+1).Value = listView1.Items[y].SubItems[x].Text;
+                                }
+                            }
+
+
+                            // lets set the header text 
+                            worksheet.HeaderFooter.oddHeader.CenteredText = "Rakapitulasi Suhu Beton";
+ 
+                            // add the page number to the footer plus the total number of pages
+                            worksheet.HeaderFooter.oddFooter.RightAlignedText =
+                                string.Format("Halaman {0} dari {1}", ExcelHeaderFooter.PageNumber, ExcelHeaderFooter.NumberOfPages);
+                            
+                            // add the sheet name to the footer
+                            worksheet.HeaderFooter.oddFooter.CenteredText = ExcelHeaderFooter.SheetName;
+                            
+                            // add the file path to the footer
+                            worksheet.HeaderFooter.oddFooter.LeftAlignedText = ExcelHeaderFooter.FilePath + ExcelHeaderFooter.FileName;
+
+                            // change the sheet view to show it in page layout mode
+                            worksheet.View.PageLayoutView = true;
+
+                            // set some core property values
+                            xlPackage.Workbook.Properties.Title = "Rakapitulasi Suhu Beton";
+                            xlPackage.Workbook.Properties.Author = "Ristiana Dyah";
+                            xlPackage.Workbook.Properties.Subject = "Lembar Rakapitulasi Suhu Beton";
+                            xlPackage.Workbook.Properties.Keywords = "Suhu Beton";
+                            xlPackage.Workbook.Properties.Category = "Laporan Rekapitulasi";
+                            xlPackage.Workbook.Properties.Comments = "Rakapitulasi Suhu Beton dari pengukuran alat pengukur suhu beton";
+
+                            // set some extended property values
+                            xlPackage.Workbook.Properties.Company = "Universitas Muhammadiyah Purwokerto";
+                            xlPackage.Workbook.Properties.HyperlinkBase = new Uri("http://www.ump.ac.id");
+
+                            // set some custom property values
+                            xlPackage.Workbook.Properties.SetCustomPropertyValue("Checked by", "Ristiana Dyah");
+                            xlPackage.Workbook.Properties.SetCustomPropertyValue("EmployeeID", "-");
+                            xlPackage.Workbook.Properties.SetCustomPropertyValue("AssemblyName", "Excel Package");
+
+                            // save our new workbook and we are done!
+                            xlPackage.Save();
+
+                        }
+                        break;
+
+                }
+
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            timer3.Interval = int.Parse(comboBox2.Text) * 1000;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "Mulai")
+            {
+                timer3.Enabled = true;
+                button2.Text = "Berhenti";
+                timer4.Enabled = true;
+            }
+            else {
+                timer3.Enabled = false;
+                button2.Text = "Mulai";
+                timer4.Enabled = false;
+
+            }
+        }
+        string[,] xxx;
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            String jam = DateTime.Now.Hour.ToString();
+            String mnt = DateTime.Now.Minute.ToString();
+            String dtk = DateTime.Now.Second.ToString();
+            
+            String parsing = textBox1.Text;
+            String[] parsingList = parsing.Split('\n');
+
+            for (int x = 0; x < parsingList.Length; x++) {
+                if (x >= 16) continue;
+                if (parsingList[x].Length > 2)
+                {
+                    int sharpParsing = int.Parse(parsingList[x].Substring(1, 2));
+
+                    arr[sharpParsing] = parsingList[x].Substring(4,5);//parsingList[x];
+                }
+            }
+
+            for (int x = 0; x < arr.Length; x++)
+            {
+                if (arr[x] == null) arr[x] = "0";
+            }
+
+            //Add first item
+            arr[0] = jam + ":" + mnt + ":"+dtk;
+            itm = new ListViewItem(arr);
+            listView1.Items.Add(itm);
+
+            //Auto scroll
+            if (checkBox1.Checked) { listView1.EnsureVisible(listView1.Items.Count - 1); }
+
+            // usage
+
+            
+            
+
+       }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Yakin akan menghapus semua data pada tabel rekapitulasi?", "Perhatian", MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+            if (dialogResult == DialogResult.Yes)
+            {
+                //do something
+                listView1.Items.Clear();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                //do something else
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            if (toolStripProgressBar1.Value < 10)
+                toolStripProgressBar1.Value++;
+            else
+                toolStripProgressBar1.Value = 0;
+        }
+
+        
+    }
+}
